2fa891dbf8ef7cad7311bddf3ee00e1b89b894f8
arnaldoaguiarp <arnaldoaguiarp@gmail.com>
2021-11-16 22:29:13 -0300

    Creating models

---
+++ db/migrate/20211117010850_create_operations.rb
@@ -0,0 +1,17 @@
+class CreateOperations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :operations do |t|
+      t.integer :categorycode, unique: true
+      t.datetime :occurrencetime
+      t.float :value
+      t.string :card
+      t.references :store, null: false, foreign_key: true
+      t.references :recipient, null: false, foreign_key: true
+
+      t.timestamps
+    end
+
+    add_index :categories, :code, unique: true
+    add_forein_key :operations, :categories, column: :categorycode, primary_key: :code
+  end
+end
