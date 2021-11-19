8d23d91ba27fd4e56d11b38af8636d97c45ee5d5
arnaldoaguiarp <arnaldoaguiarp@gmail.com>
2021-11-19 08:28:45 -0300

    Editing operation model

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,4 +1,8 @@
 class Operation < ApplicationRecord
   belongs_to :store
   belongs_to :recipient
+  belongs_to :category, foreign_key: :categorycode, primary_key: :code, optional: true
+
+  accepts_nested_attributes_for :store
+  accepts_nested_attributes_for :recipient
 end
